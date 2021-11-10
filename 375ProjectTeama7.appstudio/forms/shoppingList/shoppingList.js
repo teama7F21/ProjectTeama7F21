f8d80f8041da3e82adfe30b43ad8d3e4589c1334
bridgetclifford <90580691+bridgetclifford@users.noreply.github.com>
2021-11-10 12:56:57 -0600

    shoppingList

---
+++ 375ProjectTeama7.appstudio/forms/shoppingList/shoppingList.js
@@ -0,0 +1,31 @@
+let req = ""
+let query = ""
+let results = ""
+let pw = "Cr8tonbluj!" 
+let netID = "brc34170"
+let databaseSchema = "375groupa7"
+let allGroceryData = []s
+
+
+btnList.onclick=function(){
+    query = "SELECT * FROM grocery"
+    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + databaseSchema + "&query=" + query)
+
+    if (req.status == 200) { //transit trip worked.        
+        results = JSON.parse(req.responseText)
+        let message = ""
+        for (i = 0; i < results.length; i++)
+            message = message + results[i][1] + "\n"
+        txtaGrocery.value = message
+     }
+}
+
+
+btnSubmit.onclick=function(){
+    let name = inptItem.value
+    let query = "INSERT INTO grocery (`description`) VALUES ('" + name + "')"
+    //alert(query)
+    
+    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + databaseSchema + "&query=" + query)
+
+}
